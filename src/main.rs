8ab1ab799224ff039c7622cf028267f81c5265c6
Joel Nises <joel.nises@gmail.com>
2022-11-27 18:15:33 +0100

    improved error handling

--- src/main.rs
+++ src/main.rs
@@ -70,19 +70,20 @@ struct Contributions {
 }
 
 impl Contributions {
-    // TODO max commit age arg?
     fn try_from_path(
         repo: &Repository,
         path: &Path,
         max_age: &Option<chrono::Duration>,
     ) -> Result<Self> {
         let blame = repo.blame_file(path, Some(BlameOptions::new().use_mailmap(true)))?;
-        Ok(blame.iter().fold(Self::default(), |mut acc, hunk| {
+        let mut s = Self::default();
+        for hunk in blame.iter() {
             let lines = hunk.lines_in_hunk();
             let signature = hunk.final_signature();
             let when = signature.when();
             let commit_time = DateTime::<FixedOffset>::from_local(
-                NaiveDateTime::from_timestamp_opt(when.seconds(), 0).expect("valid timestamp"),
+                NaiveDateTime::from_timestamp_opt(when.seconds(), 0)
+                    .ok_or_else(|| anyhow!("Unable to convert commit time"))?,
                 FixedOffset::east_opt(when.offset_minutes() * 60).unwrap_or_else(|| {
                     // TODO handle error better?
                     warn!(
@@ -96,18 +97,18 @@ impl Contributions {
             let age = Utc::now() - commit_time;
             if let Some(max_age) = max_age {
                 if age > *max_age {
-                    return acc;
+                    continue;
                 }
             }
             if let Some(email) = signature.email() {
-                acc.total_lines += lines;
-                *acc.authors.entry(email.to_owned()).or_default() += lines;
+                s.total_lines += lines;
+                *s.authors.entry(email.to_owned()).or_default() += lines;
             } else {
                 // TODO keep track of unauthored hunks somehow?
                 warn!("hunk without email found in {}", path.display());
             }
-            acc
-        }))
+        }
+        Ok(s)
     }
 
     // TODO `ignored_users` will probably not get large enough to warrant a HashSet?
@@ -366,12 +367,10 @@ fn main() -> Result<()> {
             .ok_or_else(|| anyhow!("bad email configured"))?
             .to_string()]
     };
-    let max_age = opt
-        .max_age
-        .map(|d| {
-            info!("max age: {d}");
-            chrono::Duration::from_std(d.into()).expect("bad duration")
-        });
+    let max_age = opt.max_age.map(|d| {
+        info!("max age: {d}");
+        chrono::Duration::from_std(d.into()).expect("bad duration")
+    });
     info!("Looking for lines made by email(s) {emails:?}");
     let head = repo.head()?.peel_to_tree()?;
     let progress = if opt.no_progress || opt.verbose > 0 {
